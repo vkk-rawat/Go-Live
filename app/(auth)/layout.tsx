d2c30ed45fafddb113bcda7a31193b2b95827c30
NoObXxProgrammer <vickyrawat5998@gmail.com>
2024-01-03 20:47:01 +0530

    add files

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+const AuthLayout = ({
+    children
+}: {
+    children: React.ReactNode;
+}) => {
+    return (
+        <div className="h-full flex items-center">
+            {children}
+        </div>
+    );
+};
+
+export default AuthLayout;
